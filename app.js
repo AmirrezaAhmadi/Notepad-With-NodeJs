cb450bed9e5015184eda6a2e9120bb51c80075da
AmirrezaAhmadi <AmirrezaAhmadi.GH@gmail.com>
2024-10-26 09:45:14 +0330

    model and DataBase Added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require("path");
 const notepadRouter=require('./router/notepadRouter');
 const bodyParser = require('body-parser');
+const Note = require('./models/notebook');
+const mongoose = require('mongoose');
 
 require("dotenv").config();
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,6 +13,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(notepadRouter);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 const port = process.env.PORT;
 app.listen(port, () => {
